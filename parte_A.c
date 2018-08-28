afbcca4535efec6436d6f54a13c78572a717c89c
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2018-08-27 20:57:44 -0500

    Parte A

---
+++ parte_A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Libro
+{
+	char *titulo;
+	int anioDePublicacion;
+	struct Libro *next;
+};
+
+int main (int argc, char *argv[])
+{
+	struct Libro *lista;
+	/* Pon aqui tu codigo */
+	return 0;
+}
