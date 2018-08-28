7bc411e2fd15b5bb9daee59a8838df655a96d326
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2018-08-27 21:02:09 -0500

    Parte B

---
+++ parte_B.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int sumar (int *elementos)
+{
+	/* Mi funcion recursiva */
+}
+
+int main (int argc, char *argv[])
+{
+	int elementos[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int resultado = sumar(elementos);
+	return 0;
+}
