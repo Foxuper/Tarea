ef5a454d3fb836d8a03cb2bb48797e6fa84db7f7
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2018-08-27 21:14:12 -0500

    Parte B

--- parte_B.c
+++ parte_B.c
@@ -2,12 +2,17 @@
 
 int sumar (int *elementos)
 {
-	/* Mi funcion recursiva */
+	if (elementos[0] == 10)
+		return elementos[0];
+	else
+		return elementos[0] + sumar(&elementos[1]);
 }
 
 int main (int argc, char *argv[])
 {
 	int elementos[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int resultado = sumar(elementos);
+	printf("Suma = %d", resultado);
+	getchar();
 	return 0;
 }
