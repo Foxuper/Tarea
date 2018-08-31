d9ca793c94a14495ca0501bf759fb258d43471a2
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2018-08-31 16:11:49 -0500

    Parte B

--- parte_B.c
+++ parte_B.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
-int sumar (int *elementos)
+// Sumar | Argumentos //
+// *elementos - Apuntador a un arreglo
+// n          - Cantidad de elementos en el arreglo
+int sumar (int *elementos, int n)
 {
-	if (elementos[0] == 10)
+	if (n == 1)
 		return elementos[0];
 	else
-		return elementos[0] + sumar(&elementos[1]);
+		return elementos[0] + sumar(&elementos[1], n - 1);
 }
 
-int main (int argc, char *argv[])
+// Main | Argumentos //
+// void - No son necesarios argumentos
+int main (void)
 {
 	int elementos[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int resultado = sumar(elementos);
+	int resultado = sumar(elementos, 10);
 	printf("Suma = %d", resultado);
 	getchar();
 	return 0;
 }
+
