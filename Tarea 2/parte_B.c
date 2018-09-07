6189a9f406c76b5a8a87391bb30bc3a7967d173e
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2018-09-07 17:59:46 -0500

    Tarea 2 - Parte B

---
+++ Tarea 2/parte_B.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Tipo de dato | Booleano //
+typedef int bool;
+#define true 1
+#define false 0
+
+int main (void)
+{
+	return 0;
+}
