e0de92d977208ac49d8881fe1387007749cc3e08
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2018-10-12 23:15:15 -0500

    Tarea 6

---
+++ Tarea 6/parte_A.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tipo de dato | Booleano //
+typedef int bool;
+#define true 1
+#define false 0
+
+// Tipo de dato | Nodo //
+typedef struct Nodo
+{
+	int valor;
+	struct Nodo *left;
+	struct Nodo *right;
+} Nodo;
+
+// Tipo de dato | Cola //
+typedef struct Arbol
+{
+	Nodo *raiz;
+} Arbol;
+
+// Definiciones | Funciones de Memoria //
+Nodo  *crear_Nodo       (int valor);
+Arbol *crear_Arbol      (void);
+void   borrar_Arbol     (Arbol *arbol);
+void   borrar_Arbol_Aux (Nodo *raiz);
+
+// Definiciones | Funciones //
+void insertar            (Arbol *arbol, int valor);
+void insertar_Aux        (Nodo *raiz, Nodo *nodo);
+void recorrido_Preorden  (Nodo *raiz);
+void recorrido_Inorden   (Nodo *raiz);
+void recorrido_Postorden (Nodo *raiz);
+
+// Funcion de Memoria | Crear Nodo //
+Nodo *crear_Nodo (int valor)
+{
+	Nodo *nodo = malloc(sizeof(Nodo));
+	nodo->valor = valor;
+	nodo->left = NULL;
+	nodo->right = NULL;
+	return nodo;
+}
+
+// Funcion de Memoria | Crear Arbol //
+Arbol *crear_Arbol (void)
+{
+	Arbol *arbol = malloc(sizeof(Arbol));
+	arbol->raiz = NULL;
+	return arbol;
+}
+
+// Funcion de Memoria | Borrar Arbol //
+void borrar_Arbol (Arbol *arbol)
+{
+	if (arbol->raiz != NULL)
+		borrar_Arbol_Aux(arbol->raiz);
+	free(arbol);
+}
+
+// Funcion de Memoria | Auxiliar de Borrar Arbol //
+void borrar_Arbol_Aux (Nodo *raiz)
+{
+	if (raiz->left != NULL)
+		borrar_Arbol_Aux(raiz->left);
+	if (raiz->right != NULL)
+		borrar_Arbol_Aux(raiz->right);
+	free(raiz);
+}
+
+// Funcion | Insertar //
+void insertar (Arbol *arbol, int valor)
+{
+	Nodo *nodo = crear_Nodo(valor);
+	
+	if (arbol->raiz != NULL)
+		insertar_Aux(arbol->raiz, nodo);
+	else
+		arbol->raiz = nodo;
+}
+
+// Funcion | Auxiliar de Insertar //
+void insertar_Aux (Nodo *raiz, Nodo *nodo)
+{
+	int diferencia = nodo->valor - raiz->valor;
+	
+	if (diferencia < 0)
+	{
+		if (raiz->left != NULL)
+			insertar_Aux(raiz->left, nodo);
+		else
+			raiz->left = nodo;
+	}
+	
+	if (diferencia > 0)
+	{
+		if (raiz->right != NULL)
+			insertar_Aux(raiz->right, nodo);
+		else
+			raiz->right = nodo;
+	}
+}
+
+int altura (Nodo *raiz)
+{
+	if (raiz != NULL)
+	{
+		int altura_left = altura(raiz->left);
+		int altura_right = altura(raiz->right);
+		
+		if (altura_left > altura_right)
+			return altura_left + 1;
+		else
+			return altura_right + 1;
+	}
+	else
+		return 0;
+}
+
+int balance (Nodo *raiz)
+{
+	if (raiz != NULL)
+		return altura(raiz->left) - altura(raiz->right);
+	else
+		return 0;
+}
+
+Nodo *rotar_left (Nodo *raiz)
+{
+	Nodo *pivote = raiz->right;
+	pivote->left = raiz;
+	return pivote;
+}
+
+Nodo *rotar_right (Nodo *raiz)
+{
+	Nodo *pivote = raiz->left;
+	pivote->right = raiz;
+	return pivote;
+}
+
+Nodo *rotar_left_right (Nodo *a)
+{
+	Nodo *b = a->left;
+	Nodo *c = b->right;
+	
+	rotar_left(b);
+	rotar_right(a);
+	
+	return c;
+}
+
+Nodo *rotar_right_left (Nodo *a)
+{
+	Nodo *b = a->right;
+	Nodo *c = b->left;
+	
+	rotar_right(b);
+	rotar_left(a);
+	
+	return c;
+}
+
+void insertar_avl (Arbol *arbol, int valor)
+{
+	Nodo *nodo = crear_Nodo(valor);
+	
+	if (arbol->raiz != NULL)
+		insertar_avl_aux(arbol->raiz, nodo);
+	else
+		arbol->raiz = nodo;
+}
+
+Nodo *insertar_avl_aux (Nodo *raiz, Nodo *nodo)
+{
+	if (raiz == NULL)
+		return nodo;
+		
+	int diferencia = nodo->valor - raiz->valor;
+	
+	if (diferencia < 0)
+		raiz.left = insertar_avl_aux(raiz.left, nodo);
+	else
+		raiz.right = insertar_avl_aux(raiz.right, nodo);
+		
+	int d_balance = balance(raiz);
+	
+	if ((d_balance > 1) && (valor < raiz.left.valor))
+		return rotar_right(raiz);
+		
+	if ((d_balance > 1) && (valor < raiz.left.valor))
+		return rotar_right(raiz);
+}
+
+// Funcion | Recorrido Preorden //
+void recorrido_Preorden (Nodo *raiz)
+{
+	printf("%d ", raiz->valor);
+	
+	if (raiz->left != NULL)
+		recorrido_Preorden(raiz->left);
+	
+	if (raiz->right != NULL)
+		recorrido_Preorden(raiz->right);
+}
+
+// Funcion | Recorrido Inorden //
+void recorrido_Inorden (Nodo *raiz)
+{
+	if (raiz->left != NULL)
+		recorrido_Inorden(raiz->left);
+		
+	printf("%d ", raiz->valor);
+	
+	if (raiz->right != NULL)
+		recorrido_Inorden(raiz->right);
+}
+
+// Funcion | Recorrido Postorden //
+void recorrido_Postorden (Nodo *raiz)
+{
+	if (raiz->left != NULL)
+		recorrido_Postorden(raiz->left);
+	
+	if (raiz->right != NULL)
+		recorrido_Postorden(raiz->right);
+	
+	printf("%d ", raiz->valor);
+}
+
+// -------- MAIN -------- //
+int main (void)
+{
+	int valor;
+	char opcion;
+	bool salir = false;
+	Arbol *arbol = crear_Arbol();
+	
+	while (!salir)
+	{
+		system("clear || cls");
+		
+		printf("\n Preorden -- [ ");
+		if (arbol->raiz != NULL)
+			recorrido_Preorden(arbol->raiz);
+		printf("]");
+		
+		printf("\n Inorden --- [ ");
+		if (arbol->raiz != NULL)
+			recorrido_Inorden(arbol->raiz);
+		printf("]");
+		
+		printf("\n Postorden - [ ");
+		if (arbol->raiz != NULL)
+			recorrido_Postorden(arbol->raiz);
+		printf("]");
+		
+		printf("\n\n 1) Insertar");
+		printf("\n 2) Reiniciar");
+		printf("\n 3) Salir");
+		printf("\n\n Opcion: ");
+		
+		opcion = getchar();
+		
+		switch (opcion)
+		{
+			case '1':
+				printf("\n Ingrese un valor a insertar: ");
+				scanf("%d", &valor);
+				insertar(arbol, valor);
+			break;
+			case '2':
+				borrar_Arbol(arbol);
+				arbol = crear_Arbol();
+			break;
+			case '3':
+				printf("\n Presione una tecla para salir . . .");
+				salir = true;
+				getchar();
+			break;
+			default:
+				printf("\n Opcion invalida . . .");
+				getchar();
+			break;
+		}
+		
+		getchar();
+	}
+	
+	borrar_Arbol(arbol);
+	
+	return 0;
+}
